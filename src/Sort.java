a24468c72d46e79ebbd681da662ab879ad289ecf
rwolff2s <92735150+rwolff2s@users.noreply.github.com>
2022-04-07 18:14:16 +0200

    Klappt das?

---
+++ src/Sort.java
@@ -0,0 +1,14 @@
+
+public class Sort implements SortIF {
+
+	public Sort(String [][] ruleset) {
+		
+	}
+	
+	@Override
+	public boolean isWellSorted(String[] sequence) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
