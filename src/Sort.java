93bcfb503b225f69791d3fcca7b0595e97f3caaa
rwolff2s <92735150+rwolff2s@users.noreply.github.com>
2022-04-08 02:18:47 +0200

    Sort Commit

--- src/Sort.java
+++ src/Sort.java
@@ -1,14 +1,36 @@
 
 public class Sort implements SortIF {
+	private String[][] ruleset;
+	
 
 	public Sort(String [][] ruleset) {
-		
+		this.ruleset = ruleset;
 	}
+
 	
 	@Override
-	public boolean isWellSorted(String[] sequence) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean isWellSorted(String[] s) {
+		int t = 0;
+		for(int i=0; i<s.length; i++) {
+			for(int a=0;a<ruleset.length; a++) {
+				if(s[i].equals(ruleset[a][1])) {
+					for(int b=0;b<i;b++) {
+						if(ruleset[a][0].equals(s[b])) {
+							t=1;
+						}
+					}
+					if(t==0) {
+						System.out.println(a + " " + i);
+						return false;
+					}else {
+						t =0;
+					}
+				}
+			}
+			
+		}
+				
+		return true;
 	}
 
 }
